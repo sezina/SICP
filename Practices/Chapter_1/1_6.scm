ae24c8ff50add6b33835acca2c2adc9c3395cfea
sezina <sezinamd@gmail.com>
2014-02-23 15:09:14 +0800

    add: 1_6.scm

---
+++ Practices/Chapter_1/1_6.scm
@@ -0,0 +1,4 @@
+; It will cause an Aborting! Because maximum recursion depth exceeded.
+; new-if is a procedure. When evaluate this procedure, the interpreter 
+; whill evaluate all of its' arguments first. Thus, it will generate an 
+; aborting that maximum recursion depth exceeded.

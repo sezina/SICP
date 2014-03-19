101c0b91337d036c6a45ab239db82787c3c91179
sezina <sezinamd@gmail.com>
2014-03-19 22:36:28 +0800

    rm: 1_26.scm

--- Practices/Chapter_1/1_26.scm
+++
@@ -1,4 +0,0 @@
-; The expression of the second condition, 
-; (remainder (* (expmod ...) (expmod ...)) m),
-; it will evaluate (expmod ...) before apply `*` and evaluate remainder.
-; Because (expmod ...) is a procedure.

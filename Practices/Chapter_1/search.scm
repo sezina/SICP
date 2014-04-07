593b49eaec9b21afea7a5a2d4c0c0b6a995c3e32
sezina <sezinamd@gmail.com>
2014-04-07 13:27:49 +0800

    add: search.scm fixed-point.scm

---
+++ Practices/Chapter_1/search.scm
@@ -0,0 +1,13 @@
+(load "average.scm")
+(load "close-enough.scm")
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+      midpoint
+      (let ((test-value (f midpoint)))
+        (cond ((positive? test-value)
+               (search f neg-point midpoint))
+              ((negative? test-value)
+               (search f midpoint pos-point))
+              (else midpoint))))))

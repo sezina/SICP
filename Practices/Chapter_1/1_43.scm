7a72efb07c4627e7d6cd050fd2dab2ea0518baad
sezina <sezinamd@gmail.com>
2014-04-14 10:56:41 +0800

    update: 1_43.scm

--- Practices/Chapter_1/1_43.scm
+++ Practices/Chapter_1/1_43.scm
@@ -1,9 +1,9 @@
 (load "1_42.scm")
 
 (define (repeated f times)
-  (lambda (x)
     (if (= times 1)
-      (f x)
-      ((compose (repeated f (- times 1)) f) x))))
+      f 
+      (compose f 
+               (repeated f (- times 1)))))
 
 ((repeated square 2) 5)  ; => 625

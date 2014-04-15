d2270066d7cd17cc03ea8f4f333cd8efce14fddc
sezina <sezinamd@gmail.com>
2014-04-15 16:01:42 +0800

    add: 2_01.scm

---
+++ Solutions/Chapter_2/2_01.scm
@@ -0,0 +1,12 @@
+;(define (make-rat n d)
+;  (let ((g (gcd (abs n) (abs d))))
+;    (if (or (and (< n 0) (> d 0)) 
+;            (and (> n 0) (< d 0))) 
+;      (cons (- (/ (abs n) g)) (/ (abs d) g))
+;      (cons (/ (abs n) g) (/ (abs d) g)))))
+
+(define (make-rat n d)
+  (let ((g (gcd (abs n) (abs d))))
+    (if (< (* n d) 0) 
+      (cons (- (/ (abs n) g)) (/ (abs d) g))
+      (cons (/ (abs n) g) (/ (abs d) g)))))

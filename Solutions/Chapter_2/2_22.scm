418cf10d624e3458172304c839979b5146f1974b
sezina <sezinamd@gmail.com>
2014-06-23 15:50:43 +0800

    add: 2_22.scm

---
+++ Solutions/Chapter_2/2_22.scm
@@ -0,0 +1,19 @@
+(define (square x) (* x x))
+(define (square-list1 items)
+  (define (iter things answer)
+    (if (null? things)
+      answer
+      (iter (cdr things)
+            (cons (square (car things))
+                  answer))))
+  (iter items '()))
+
+
+(define (square-list2 items)
+  (define (iter things answer)
+    (if (null? things)
+      answer
+      (iter (cdr things)
+            (cons answer
+                  (square (car things))))))
+  (iter items '()))

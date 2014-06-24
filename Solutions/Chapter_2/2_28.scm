7b5e4a635a7fb23e143dd10215291350e8fcd31c
sezina <sezinamd@gmail.com>
2014-06-24 13:17:07 +0800

    add: 2_28.scm

---
+++ Solutions/Chapter_2/2_28.scm
@@ -0,0 +1,20 @@
+(define x (list (list 1 2) (list 3 4)))
+
+(newline)
+(display "x: ")
+(display x)
+
+(define (fringe x)
+  (define (iter items ans)
+    (cond ((null? items) ans)
+          ((not (pair? items)) (append ans (list items)))
+          (else (append (iter (car items) ans)
+                        (iter (cdr items) ans)))))
+  (iter x '()))
+
+(newline)
+(display "(fringe x): ")
+(display (fringe x))
+(newline)
+(display "(fringe (list x x)): ")
+(display (fringe (list x x)))
